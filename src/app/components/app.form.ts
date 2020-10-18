bd2beb80291e2aba6e1f54b802142685187ba54b
André Marques <andre.marques@metropoles.com>
2020-10-18 12:25:14 -0300

    style ticket component and create request for CEP

--- src/app/components/app.form.ts
+++ src/app/components/app.form.ts
@@ -31,16 +31,24 @@ export class AppForm {
     phone: ['']
   });
 
-  constructor(public httpClient: HttpClient, private fb: FormBuilder){}
+  constructor (
+    public httpClient: HttpClient,
+    private fb: FormBuilder
+  ){}
 
-  sendCepRequest(){
-    this.httpClient.get('https://correiosapi.apphb.com/').subscribe((res)=>{
-        console.log(res);
+  getCEP(cep) {
+    this.httpClient.get('https://viacep.com.br/ws/' + cep + '/json/').subscribe((res)=>{
+      console.log(res);
     });
   }
 
+  sendCepRequest(e){
+    if (this.form.get('cep').valid) {
+      this.getCEP(e.target.value)
+    }
+  }
+
   onSubmit() {
-    // TODO: Use EventEmitter with form value
     console.warn(this.form.value);
   }
 }
--- src/app/components/app.ticket.ts
+++ src/app/components/app.ticket.ts
@@ -9,11 +9,22 @@ import { HttpClient } from '@angular/common/http';
 
 export class AppTicket {
   title = 'Ingresso';
+  api = 'https://developers.themoviedb.org/3/movies/get-upcoming';
 
   constructor(private httpClient: HttpClient){}
 
+  options = {
+    responseType: 'json',
+    withCredentials: false,
+    params: {
+      api_key: '4a84a8b77f84df64cc6ec6a9a2335f0b',
+      page: 1,
+      region: 'ISO3166-2'
+    }
+  }
+
   ngOnInit() {
-    this.httpClient.get('https://developers.themoviedb.org/3/movies/get-upcoming?api_key=4a84a8b77f84df64cc6ec6a9a2335f0b&page=1&region=ISO3166-2:BR', { withCredentials: true }).subscribe((res)=>{
+    this.httpClient.get(this.api).subscribe((res)=>{
       console.log(res);
     });
   }
